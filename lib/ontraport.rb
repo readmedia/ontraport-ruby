1c9049a386e8c1c5e830abf069f6cdf8d841686e
hamzatayeb <hamza.tayeb@onetaste.us>
2016-02-04 19:38:41 +0000

    add save_or_update method

--- lib/ontraport.rb
+++ lib/ontraport.rb
@@ -2,7 +2,8 @@ module Ontraport
   BASE_URL = 'https://api.ontraport.com/'
   API_VERSION = '1'
 
-  # --- configuration ---
+  attr_accessor :objects_meta_cache
+
   def self.configuration
     @configuration ||= Configuration.new
   end
@@ -15,24 +16,53 @@ module Ontraport
     attr_accessor :api_id
     attr_accessor :api_key
   end
-  # --- /end configuration ---
 
   class APIError < StandardError
   end
 
-  def self.objects_meta
-    request_with_authorization :get, endpoint: '/objects/meta'
+  class ObjectNotFoundError < StandardError
   end
 
-  private
-    def self.request_with_authorization method, endpoint:, data: nil
-      resp =  HTTParty.send method, "#{BASE_URL}#{API_VERSION}#{endpoint}",
-        headers: { 'Api-Appid' => @configuration.api_id, 'Api-Key' => @configuration.api_key }
+  def self.describe object
+    unless metadata = objects_meta['data']&.find{ |k, v| v['name'].downcase.to_sym.eql? object }
+      raise ObjectNotFoundError.new "No object matching #{object.inspect} could be found"
+    end
 
-      unless resp.response.code.eql? '200'
-        raise APIError.new resp.response.body
+    metadata.second.update 'object_id' => metadata.first
+  end
+
+  # --- get calls ---
+  def self.objects_meta
+    @objects_meta_cache ||= request_with_authentication :get, endpoint: '/objects/meta'
+  end
+
+  # --- post calls ---
+  def self.save_or_update object, params
+    metadata = describe object
+
+    params.update 'objectID' => metadata['object_id']
+    request_with_authentication :post, endpoint: '/objects/saveorupdate', data: params
+  end
+
+  # --- put calls ---
+  # --- delete calls ---
+
+  private
+    def self.request_with_authentication method, endpoint:, data: nil
+      data_param = method.in?([:post, :put]) ? :body : :query
+
+      args = [method, "#{BASE_URL}#{API_VERSION}#{endpoint}"]
+      kwargs = {
+        :headers => { 'Api-Appid' => @configuration.api_id, 'Api-Key' => @configuration.api_key },
+        data_param => data
+      }
+
+      response =  HTTParty.send *args, **kwargs
+
+      unless response.code.eql? 200
+        raise APIError.new response.body
       end
 
-      resp.parsed_response
+      response.parsed_response
     end
 end
